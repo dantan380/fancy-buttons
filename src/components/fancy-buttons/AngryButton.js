0795afe49a9b8cf693966c11b7444a4c8b898359
Daniel Tan <danieltan380@gmail.com>
2023-10-31 17:20:50 -0700

    updated AngryButton to useReducer instead of useState

--- src/components/fancy-buttons/AngryButton.js
+++ src/components/fancy-buttons/AngryButton.js
@@ -1,17 +1,15 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 const AngryButton = () => {
-  const [anger, setAnger] = useState(0);
 
-  const handleClick = (amount) => {
+  const [anger, dispatch] = useReducer((anger, action) => {
     if (anger < 1) {
-      setAnger(anger + amount);
-    } else {
-      setAnger(0);
-    }
-  }
+      return anger > 1 ? 0 : anger + action;
+    } 
+  }, 0)
+
   return (
-    <button className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}} onClick={() => {handleClick(0.1)}}>
+    <button className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}} onClick={() => {dispatch(0.1)}}>
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
--- src/components/fancy-buttons/LightSwitchButton.js
+++ src/components/fancy-buttons/LightSwitchButton.js
@@ -1,10 +1,8 @@
 const LightSwitchButton = (props) => {
-  const {light, setLight} = props;
-
-  const handleClick = () => setLight(light === "on" ? "off" : "on")
+  const {light, switchLight} = props;
 
   return (
-    <button className="LightSwitchButton" onClick={handleClick}>
+    <button className="LightSwitchButton" onClick={switchLight}>
       {light === "on" && <span><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
